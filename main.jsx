70f4594755ba87a0de67cf3b8d6bdd2998f6170f
Tine Marienhof Hyllested <tinemarienhof@gmail.com>
2026-04-27 19:40:06 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -2,6 +2,5 @@ jsximport React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  React.createElement(React.StrictMode, null, React.createElement(App))
-)
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(React.createElement(App))
